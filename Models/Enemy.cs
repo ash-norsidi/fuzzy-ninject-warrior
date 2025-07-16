55e4db6912f743d5776e14aa539ea94dbcae0236
Ashi <ash.norsidi@gmail.com>
2025-07-16 12:35:24 +0800

    Update Enemy.cs

--- Models/Enemy.cs
+++ Models/Enemy.cs
@@ -8,6 +8,6 @@ namespace FuzzyNinjectWarrior.Models
         public int Id { get; set; }
         public string Name { get; set; }
         public int Health { get; set; }
-        public int Attack { get; set; }
+        public int Level { get; set; }
     }
 }
