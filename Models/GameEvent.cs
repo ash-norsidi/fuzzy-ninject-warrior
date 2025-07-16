5efbbe4f09d71ff8af3f7c265315b9cfbd37d87f
Ashi <ash.norsidi@gmail.com>
2025-07-16 13:55:30 +0800

    Update GameEvent.cs

--- Models/GameEvent.cs
+++ Models/GameEvent.cs
@@ -1,13 +1,25 @@
 using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace NinjectWarriorAdventure.Models
 {
     public class GameEvent
     {
+        [Key]
         public int Id { get; set; }
+
         public DateTime Timestamp { get; set; } = DateTime.UtcNow;
+
+        [Required]
         public string Description { get; set; }
+
         public int? PlayerId { get; set; }
+        [ForeignKey("PlayerId")]
+        public Player Player { get; set; }
+
         public int? EnemyId { get; set; }
+        [ForeignKey("EnemyId")]
+        public Enemy Enemy { get; set; }
     }
 }
